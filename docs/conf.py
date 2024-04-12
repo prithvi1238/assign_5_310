33cc66cfe367ed2922f657d78019b538bcfb5c49
Prithvi Sureka <prithvisureka@hotmail.com>
2024-04-11 18:17:30 -0700

    made changes to the wrong repo

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,6 @@ extensions = [
 autoapi_dirs = ["../src"]
 
 # List of patterns, relative to source directory, that match files and
-# This pattern also affects html_static_path and html_extra_path.
 # directories to ignore when looking for source files.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
